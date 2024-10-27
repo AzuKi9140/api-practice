9604651cab9f1b07f558c904163f3d37e7759c46
AZUKI\kazuk <kazuki20029140@gmail.com>
2024-10-27 10:01:53 +0900

    view部分を追加

--- flask_app/flaskr/__init__.py
+++ flask_app/flaskr/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 
 from flaskr.api import api_bp
+from flaskr.index import index_bp
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
+    app.register_blueprint(index_bp)
     app.register_blueprint(api_bp)
 
     return app
